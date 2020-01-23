fa8683029e18396184307931cb2c4c3a46a53f5e
Alexander <alex1812r@gmail.com>
2020-01-23 19:15:53 -0400

    conectar a mongo atlas

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const url =  
-  // 'https://usuariocrudbackend.herokuapp.com/api';  
-  'http://localhost:5000/api';
+  'https://usuariocrudbackend.herokuapp.com/api';  
+  // 'http://localhost:5000/api';
 
 const clientAxios = axios.create({ baseURL: url });
 export default clientAxios;
